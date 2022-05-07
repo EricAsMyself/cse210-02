4514b9c72bde4b1a9e860b040785d33c1d58b2cf
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2022-05-07 11:33:31 -0600

    added class and the first couple of functions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Changethis{
+    class ChangeNameP{
+        private string givenname;
+        private string familyname;
+        private string easter_name;
+        private string western_name;
+        
+        public ChangeNameP(string fname, string lname){
+            givenname = fname;
+            familyname = lname;
+        }
+        public void make_easter_name(){
+           easter_name = givenname; 
+        }
+        public void show_easter_name(){
+            Console.WriteLine($"{givenname} {familyname}");
+        }
+        public
+        }
+    }
+}
