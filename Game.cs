acab9d4de938a58408bb6af984f53df36054377e
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2022-05-11 10:55:01 -0600

    Co-authored-by: Spencer Saunders <spencersaunders45@users.noreply.github.com> Co-authored-by: GJohnston425 <GJohnston425@users.noreply.github.com> Co-authored-by: Tart, Jacob Loren Eli <tar21006@byui.edu>

--- Game.cs
+++ Game.cs
@@ -18,25 +18,30 @@ namespace cse210{
             /// </summary>
             /// <returns> in loop until finnished</returns>
             while (keepPlaying){
-                Console.WriteLine($"the card is :{_gameInformation.GetCurrentCard()}");
-                HigherLower();
-                Console.WriteLine($"next card was :{_gameInformation.GetNextCard()}");
-                Console.WriteLine($"Your current score is :{_gameInformation.GetScore()}");               
+                Console.WriteLine($"the card is : {_gameInformation.GetCurrentCard()}");
+                HigherLower(Playing);
+                Console.WriteLine($"next card was : {_gameInformation.GetNextCard()}");
+                Console.WriteLine($"Your current score is : {_gameInformation.GetScore()}");               
                 keepPlaying = UserInputContinue();
                 _gameInformation.SetNextCard();
+                Console.WriteLine();
             }
 
         }
         
-        public void HigherLower(){
+        public void HigherLower(bool keepPlaying){
             /// <summary>
             /// Check's score, and compares against the user input
             /// </summary>
             /// <setscore>int</setscore>
-            Console.WriteLine("Higher or Lower?(h/l) :");
+            Console.WriteLine("Higher or Lower?(h/l)(To stop playing type: 'n'):");
             userInput = Console.ReadLine();
             if (userInput == "h" && _gameInformation.CheckScore() || (userInput == "l" && _gameInformation.CheckScore() == false)){
                 _gameInformation.SetScore(-75);
+            }
+            if (userInput == "n" ){
+                Console.WriteLine($"your score is {_gameInformation.GetScore()}, thanks for playing.");
+                keepPlaying = false;
             }
             else{
                 _gameInformation.SetScore(100);
